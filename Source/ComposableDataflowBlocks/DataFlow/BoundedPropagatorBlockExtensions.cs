628afdf9a4d1fe7f0e31e07e98848af13d21fb70
Robert van Herk <rvanherk@entis.ai>
2025-11-28 11:45:48 +0100

    fix

--- Source/ComposableDataflowBlocks/DataFlow/BoundedPropagatorBlockExtensions.cs
+++ Source/ComposableDataflowBlocks/DataFlow/BoundedPropagatorBlockExtensions.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks.Dataflow;
 
 namespace CounterpointCollective.DataFlow
 {
-    public static class BoundedBlockExtensions
+    public static class BoundedPropagatorBlockExtensions
     {
         public static BoundedPropagatorBlock<I, O> WithBoundedCapacity<I, O>(this IPropagatorBlock<I, O> b, int boundedCapacity)
         => new(b, boundedCapacity);
