89a881baebcaa6eb76258e8dbb5d0de58ef52fbe
Robert van Herk <rvanherk@entis.ai>
2025-11-29 17:51:50 +0100

    comments

--- Source/ComposableDataflowBlocks/DataFlow/Notifying/SourceBlockWithNotification.cs
+++ Source/ComposableDataflowBlocks/DataFlow/Notifying/SourceBlockWithNotification.cs
@@ -253,7 +253,7 @@ namespace CounterpointCollective.DataFlow.Notifying
                         }
                     } else
                     {
-                        //Source is already busy... We have to postpone.
+                        //We are already making calls into Source... We have to postpone to prevent deadlocks.
                         if (source != null) 
                         {
                             lock (_postponedMessagesManager.IncomingLock)
